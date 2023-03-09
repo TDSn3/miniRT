9d4e7feb99957ffd292dd21c115d0dce50b95e06
Roberto Catini <rcatini@student.42.fr>
2023-03-09 04:07:09 +0100

    more compact transpose

--- src/matrix/transpose_matrix4.c
+++ src/matrix/transpose_matrix4.c
@@ -6,7 +6,7 @@
 /*   By: roberto <roberto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:58:48 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/03/08 21:01:39 by roberto          ###   ########.fr       */
+/*   Updated: 2023/03/09 04:06:41 by roberto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,12 @@ t_matrix4	*transpose_matrix4(t_matrix4 const *a, t_matrix4 *dst)
 	size_t		i;
 	size_t		j;
 
-	i = 0;
-	j = 0;
-	ft_bzero(dst, sizeof(t_matrix4));
-	while (i < 4)
+	i = -1;
+	while (++i < 4)
 	{
-		while (j < 4)
-		{
+		j = -1;
+		while (++j < 4)
 			(*dst)[i][j] = (*a)[j][i];
-			j++;
-		}
-		j = 0;
-		i++;
 	}
 	return (dst);
 }
