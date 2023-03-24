2bc4208fc9e979ee2c1af610c4ad324782e8be2b
Thomas DA SILVA <thomas-78000@hotmail.fr>
2023-03-24 10:22:54 +0100

    orientation ok

--- source/transformation/rotation_xyz.c
+++ source/transformation/rotation_xyz.c
@@ -3,53 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   rotation_xyz.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcatini <rcatini@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 01:13:56 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/03/23 18:38:55 by rcatini          ###   ########.fr       */
+/*   Updated: 2023/03/24 08:50:02 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <header.h>
+#include <header.h>
 
-// t_matrix4	rotation_x(double deg)
-// {
-// 	t_matrix4	mtx;
-// 	double		rad;
+t_matrix4	rotation_x(double deg)
+{
+	t_matrix4	mtx;
+	double		rad;
 
-// 	rad = deg_to_rad(deg);
-// 	mtx = give_identity_matrix4();
-// 	mtx.tab[1][1] = cosf(rad);
-// 	mtx.tab[1][2] = sinf(rad) * -1;
-// 	mtx.tab[2][1] = sinf(rad);
-// 	mtx.tab[2][2] = cosf(rad);
-// 	return (mtx);
-// }
+	rad = deg_to_rad(deg);
+	mtx = give_identity_matrix4();
+	mtx.tab[1][1] = cosf(rad);
+	mtx.tab[1][2] = sinf(rad) * -1;
+	mtx.tab[2][1] = sinf(rad);
+	mtx.tab[2][2] = cosf(rad);
+	return (mtx);
+}
 
-// t_matrix4	rotation_y(double deg)
-// {
-// 	t_matrix4	mtx;
-// 	double		rad;
+t_matrix4	rotation_y(double deg)
+{
+	t_matrix4	mtx;
+	double		rad;
 
-// 	rad = deg_to_rad(deg);
-// 	mtx = give_identity_matrix4();
-// 	mtx.tab[0][0] = cosf(rad);
-// 	mtx.tab[0][2] = sinf(rad);
-// 	mtx.tab[2][0] = sinf(rad) * -1;
-// 	mtx.tab[2][2] = cosf(rad);
-// 	return (mtx);
-// }
+	rad = deg_to_rad(deg);
+	mtx = give_identity_matrix4();
+	mtx.tab[0][0] = cosf(rad);
+	mtx.tab[0][2] = sinf(rad);
+	mtx.tab[2][0] = sinf(rad) * -1;
+	mtx.tab[2][2] = cosf(rad);
+	return (mtx);
+}
 
-// t_matrix4	rotation_z(double deg)
-// {
-// 	t_matrix4	mtx;
-// 	double		rad;
+t_matrix4	rotation_z(double deg)
+{
+	t_matrix4	mtx;
+	double		rad;
 
-// 	rad = deg_to_rad(deg);
-// 	mtx = give_identity_matrix4();
-// 	mtx.tab[0][0] = cosf(rad);
-// 	mtx.tab[0][1] = sinf(rad) * -1;
-// 	mtx.tab[1][0] = sinf(rad);
-// 	mtx.tab[1][1] = cosf(rad);
-// 	return (mtx);
-// }
+	rad = deg_to_rad(deg);
+	mtx = give_identity_matrix4();
+	mtx.tab[0][0] = cosf(rad);
+	mtx.tab[0][1] = sinf(rad) * -1;
+	mtx.tab[1][0] = sinf(rad);
+	mtx.tab[1][1] = cosf(rad);
+	return (mtx);
+}
--- source/util/init_object.c
+++ source/util/init_object.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_object.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rcatini <rcatini@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:01:13 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/03/23 19:45:36 by rcatini          ###   ########.fr       */
+/*   Updated: 2023/03/24 10:18:08 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,79 +44,123 @@ int	init_object(t_all_data *all_data, t_dp *data_parsing)
 	return (0);
 }
 
+//static void	init_sphere(t_parsed_object *cpy, t_object *last_object)
+//{
+//	printf(" SPHERE%s\n", COLOR_RESET);
+//	last_object->transform = (t_matrix4){{
+//	{cpy->radius, 0, 0, cpy->position.x},
+//	{0, cpy->radius, 0, cpy->position.y},
+//	{0, 0, cpy->radius, cpy->position.z},
+//	{0, 0, 0, 1}
+//	}};
+//	inverse_matrix4(last_object->transform, &last_object->inverse);
+//}
+
+//t_tuple	get_non_parallel(t_tuple vector)
+//{
+//	t_tuple	non_parallel;
+
+//	if (vector.x >= vector.y && vector.x >= vector.z)
+//		non_parallel = (t_tuple){{0, 1, 0, 0}};
+//	else if (vector.y >= vector.x && vector.y >= vector.z)
+//		non_parallel = (t_tuple){{1, 0, 0, 0}};
+//	else
+//		non_parallel = (t_tuple){{0, 1, 0, 0}};
+//	return (non_parallel);
+//}
+
+//static void	init_plane(t_parsed_object *cpy, t_object *last_object)
+//{
+//	t_tuple	vertical;
+//	t_tuple	horizontal_1;
+//	t_tuple	horizontal_2;
+//	t_tuple	translation;
+
+//	vertical = (t_tuple){{cpy->direction.x, cpy->direction.y,
+//		cpy->direction.z, 0}};
+//	vertical = normalization_vector(vertical);
+//	horizontal_1 = get_non_parallel(vertical);
+//	horizontal_2 = cross_product_vector(vertical, horizontal_1);
+//	horizontal_1 = cross_product_vector(horizontal_2, vertical);
+//	translation = (t_tuple){{cpy->position.x, cpy->position.y,
+//		cpy->position.z, 0}};
+//	last_object->transform = (t_matrix4){{
+//	{horizontal_1.x, horizontal_2.x, vertical.x, translation.x},
+//	{horizontal_1.y, horizontal_2.y, vertical.y, translation.y},
+//	{horizontal_1.z, horizontal_2.z, vertical.z, translation.z},
+//	{0, 0, 0, 1}
+//	}};
+//	inverse_matrix4(last_object->transform, &last_object->inverse);
+//	last_object->material.specular = 0.1;
+//}
+
+//static void	init_cylinder(t_parsed_object *cpy, t_object *last_object)
+//{
+//	t_tuple	vertical;
+//	t_tuple	horizontal_1;
+//	t_tuple	horizontal_2;
+//	t_tuple	translation;
+
+//	vertical = (t_tuple){{cpy->direction.x, cpy->direction.y,
+//		cpy->direction.z, 0}};
+//	vertical = normalization_vector(vertical);
+//	horizontal_1 = get_non_parallel(vertical);
+//	horizontal_2 = cross_product_vector(vertical, horizontal_1);
+//	horizontal_1 = cross_product_vector(horizontal_2, vertical);
+//	vertical = t_tuple_multi_scal(vertical, cpy->height / 2);
+//	horizontal_1 = t_tuple_multi_scal(horizontal_1, cpy->radius);
+//	horizontal_2 = t_tuple_multi_scal(horizontal_2, cpy->radius);
+//	translation = (t_tuple){{cpy->position.x, cpy->position.y,
+//		cpy->position.z, 0}};
+//	last_object->transform = (t_matrix4){{
+//	{horizontal_1.x, horizontal_2.x, vertical.x, translation.x},
+//	{horizontal_1.y, horizontal_2.y, vertical.y, translation.y},
+//	{horizontal_1.z, horizontal_2.z, vertical.z, translation.z},
+//	{0, 0, 0, 1}}};
+//	inverse_matrix4(last_object->transform, &last_object->inverse);
+//	last_object->cyl_max = cpy->height / 2;
+//	last_object->cyl_min = cpy->height / 2 * -1;
+//	last_object->cyl_closed = 1;
+//}
+
 static void	init_sphere(t_parsed_object *cpy, t_object *last_object)
 {
 	printf(" SPHERE%s\n", COLOR_RESET);
-	last_object->transform = (t_matrix4){{
-	{cpy->radius, 0, 0, cpy->position.x},
-	{0, cpy->radius, 0, cpy->position.y},
-	{0, 0, cpy->radius, cpy->position.z},
-	{0, 0, 0, 1}
-	}};
+	last_object->transform = multiply_matrix4(
+			translation((t_tuple){{
+				cpy->position.x, cpy->position.y, cpy->position.z, 0}}),
+			scaling((t_tuple){{
+				cpy->radius, cpy->radius, cpy->radius, 0}}));
 	inverse_matrix4(last_object->transform, &last_object->inverse);
-}
-
-t_tuple	get_non_parallel(t_tuple vector)
-{
-	t_tuple	non_parallel;
-
-	if (vector.x >= vector.y && vector.x >= vector.z)
-		non_parallel = (t_tuple){{0, 1, 0, 0}};
-	else if (vector.y >= vector.x && vector.y >= vector.z)
-		non_parallel = (t_tuple){{1, 0, 0, 0}};
-	else
-		non_parallel = (t_tuple){{0, 1, 0, 0}};
-	return (non_parallel);
 }
 
 static void	init_plane(t_parsed_object *cpy, t_object *last_object)
 {
-	t_tuple	vertical;
-	t_tuple	horizontal_1;
-	t_tuple	horizontal_2;
-	t_tuple	translation;
-
-	vertical = (t_tuple){{cpy->direction.x, cpy->direction.y,
-		cpy->direction.z, 0}};
-	vertical = normalization_vector(vertical);
-	horizontal_1 = get_non_parallel(vertical);
-	horizontal_2 = cross_product_vector(vertical, horizontal_1);
-	horizontal_1 = cross_product_vector(horizontal_2, vertical);
-	translation = (t_tuple){{cpy->position.x, cpy->position.y,
-		cpy->position.z, 0}};
-	last_object->transform = (t_matrix4){{
-	{horizontal_1.x, horizontal_2.x, vertical.x, translation.x},
-	{horizontal_1.y, horizontal_2.y, vertical.y, translation.y},
-	{horizontal_1.z, horizontal_2.z, vertical.z, translation.z},
-	{0, 0, 0, 1}
-	}};
+	printf(" PLANE%s\n", COLOR_RESET);
+	last_object->transform = multiply_matrix4(last_object->transform,
+			translation((t_tuple){{cpy->position.x,
+				cpy->position.y, cpy->position.z, 0}}));
+	last_object->transform = multiply_matrix4(
+			last_object->transform, rotation_x(rad_to_deg(asin(cpy->direction.y))));
+	last_object->transform = multiply_matrix4(
+			last_object->transform, rotation_y(rad_to_deg(atan2(cpy->direction.x, cpy->direction.z))));
 	inverse_matrix4(last_object->transform, &last_object->inverse);
 	last_object->material.specular = 0.1;
 }
 
 static void	init_cylinder(t_parsed_object *cpy, t_object *last_object)
 {
-	t_tuple	vertical;
-	t_tuple	horizontal_1;
-	t_tuple	horizontal_2;
-	t_tuple	translation;
-
-	vertical = (t_tuple){{cpy->direction.x, cpy->direction.y,
-		cpy->direction.z, 0}};
-	vertical = normalization_vector(vertical);
-	horizontal_1 = get_non_parallel(vertical);
-	horizontal_2 = cross_product_vector(vertical, horizontal_1);
-	horizontal_1 = cross_product_vector(horizontal_2, vertical);
-	vertical = t_tuple_multi_scal(vertical, cpy->height / 2);
-	horizontal_1 = t_tuple_multi_scal(horizontal_1, cpy->radius);
-	horizontal_2 = t_tuple_multi_scal(horizontal_2, cpy->radius);
-	translation = (t_tuple){{cpy->position.x, cpy->position.y,
-		cpy->position.z, 0}};
-	last_object->transform = (t_matrix4){{
-	{horizontal_1.x, horizontal_2.x, vertical.x, translation.x},
-	{horizontal_1.y, horizontal_2.y, vertical.y, translation.y},
-	{horizontal_1.z, horizontal_2.z, vertical.z, translation.z},
-	{0, 0, 0, 1}}};
+	printf(" CYLINDER%s\n", COLOR_RESET);
+	last_object->transform = multiply_matrix4(last_object->transform,
+			translation((t_tuple){{cpy->position.x,
+				cpy->position.y, cpy->position.z, 0}}));
+	printf("%f\n", cpy->radius);
+	last_object->transform = multiply_matrix4(
+			last_object->transform, rotation_x(rad_to_deg(asin(cpy->direction.y))));
+	last_object->transform = multiply_matrix4(
+			last_object->transform, rotation_y(rad_to_deg(atan2(cpy->direction.x, cpy->direction.z))));
+	last_object->transform = multiply_matrix4(
+			last_object->transform, diameter_cylinder(cpy->radius));
 	inverse_matrix4(last_object->transform, &last_object->inverse);
 	last_object->cyl_max = cpy->height / 2;
 	last_object->cyl_min = cpy->height / 2 * -1;
